0afd7fff903cc43addf975965cd1ca96dd758faf
StastnyJ <xstastnyj@gmail.com>
2026-09-07 17:49:21 +0200

    wrong hands side

--- add_crosshairs_to_camera.py
+++ add_crosshairs_to_camera.py
@@ -5,10 +5,10 @@ def add_crosshairs_to_camera(img):
     h, w, _ = img.shape
 
     # left cross hair
-    img = cv2.circle(img, center = (w//4, h//2), radius=h//8, color=(0,0,255), thickness=3)
+    # img = cv2.circle(img, center = (w//4, h//2), radius=h//8, color=(0,0,255), thickness=3)
 
     # right cross hair
-    # img = cv2.circle(img, center = (w//4*3, h//2), radius=h//8, color=(0,0,255), thickness=3)
+    img = cv2.circle(img, center = (w//4*3, h//2), radius=h//8, color=(0,0,255), thickness=3)
 
 
     return img
