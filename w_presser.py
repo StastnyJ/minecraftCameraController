2e6e9686b411599a02f1a65fc0572b0858418b2f
StastnyJ <xstastnyj@gmail.com>
2026-09-07 17:22:55 +0200

    we can finally move in mc

---
+++ w_presser.py
@@ -0,0 +1,16 @@
+from pynput.keyboard import Controller
+import time
+
+keyboard = Controller()
+
+print("Switch to Minecraft...")
+time.sleep(5)
+
+print("Walking forward")
+keyboard.press('w')
+
+time.sleep(10)
+
+keyboard.release('w')
+
+print("Stopped")
