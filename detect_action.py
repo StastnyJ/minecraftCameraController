232272670c5ba17fd6dea69c0c8a11373fc4bdc1
Michael Ambros <ambros.michael@seznam.cz>
2026-09-08 09:16:57 +0200

    comments + fixed origin
    Co-authored-by: Jakub Šťastný <xstastnyj@gmail.com>
    Co-authored-by: ErikRadovan <ErikRadoan@users.noreply.github.com>

--- detect_action.py
+++ detect_action.py
@@ -27,39 +27,64 @@ class Gesture_Manager():
                 left = Hand(handedness[0].category_name, gesture[0].category_name, cord_xy[0].x, cord_xy[0].y)
                 self.handle_left_hand(left)
 
+            # Right hand logic
             if handedness[0].category_name == 'Right':
+
+                # Load right hand info into our hand data class
                 right = Hand(handedness[0].category_name, gesture[0].category_name, cord_xy[0].x, cord_xy[0].y)
+
+                # Check if we closed our fist
                 if self.hand_start_pos is None and right.gesture == 'Closed_Fist':
+
+                    # We release our clicks (as we cant be holding a cosed fist and a click gesture)
                     if self.hold_left_click or self.hold_right_click:
                         print("Release click")
+
+                    # Reset our click booleans 
                     self.hold_right_click = False
                     self.hold_left_click = False
+
+                    # Set starting position for our view move
                     self.hand_start_pos = (right.x_coord, right.y_coord)
+                
+                # Check for view drag release
                 if self.hand_start_pos is not None and right.gesture == 'Open_Fist':
+
+                    # Create our view drag vector
                     vec = (right.x_coord - self.hand_start_pos[0], right.y_coord - self.hand_start_pos[1])
                     self.hand_start_pos = None
                     print(*vec)
+
                     self.mouse_manager.move_mouse(vec)
+
+                # Check for left click gesture
                 if right.gesture == 'Pointing_Up':
                     self.hold_left_click = True
                     print("Left click")
                     self.mouse_manager.left_click()
+
+                # Check for right click gesture
                 if right.gesture == 'Victory':
                     self.hold_right_click = True
                     print("Right click")
                     self.mouse_manager.right_click()
+    
+    def handle_right_hand():
+        pass
             
 
     def handle_left_hand(self, left):
         """left: left hand object @ hand object"""
         # this takes care of horizontal movement (eg wasd)
         # to radial coords
-        origin = (0.25, 0.5)
+        origin = (0.75, 0.5)
         r = np.hypot(left.x_coord - origin[0], left.y_coord - origin[1])
-        theta = np.arctan2(left.y_coord - origin[1], left.x_coord - origin[0])
-        print(r, theta)
-        r2 = 0.5*101/270
-        if 0 <= r <= 0.5*67/270 and 0<= theta <=2*np.pi:
+        theta = np.arctan2(left.y_coord - origin[1], left.x_coord - origin[0]) + np.pi
+        r2 = 0.5*50/270
+
+        # print(theta/np.pi, r)
+ 
+        if 0 <= r <= 0.5*33/270 and 0<= theta <=2*np.pi:
             print('left hand is in the middle. do nothing')
         elif r < r2:
             print('do nothing; left hand in null sector')
